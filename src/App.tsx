e7fca87c83b82a057c3b8cafb88c72b085048023
krystlc <alex.valle@gmail.com>
2025-02-01 14:23:58 -0500

    feat: message time ago

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import { PrivyProvider } from "@privy-io/react-auth";
 import { toSolanaWalletConnectors } from "@privy-io/react-auth/solana";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import DovaModel from "./components/DovaModel";
+// import DovaModel from "./components/DovaModel";
 import { ChatWindow } from "./components/chat/ChatWindow";
 import AppHeader from "./components/app/AppHeader";
 
@@ -34,7 +34,7 @@ function App() {
       >
         <AppHeader />
         <ChatWindow />
-        <DovaModel />
+        {/* <DovaModel /> */}
       </PrivyProvider>
     </QueryClientProvider>
   );
--- src/components/chat/ChatWindow.tsx
+++ src/components/chat/ChatWindow.tsx
@@ -1,95 +1,39 @@
 import { Message, useChat } from "@/hooks/useChat";
 import ChatInput from "./ChatInput";
 import { useEffect, useMemo, useRef } from "react";
+import { formatDistance, formatDistanceToNow } from "date-fns";
 
 export const ChatWindow = () => {
-  const { error, messages, isFetched } = useChat();
+  const { error, messages, isLoading } = useChat();
 
   const chatLog = useMemo(() => {
     return messages.sort((a, b) => a.timestamp - b.timestamp);
   }, [messages]);
 
-  const renderMessage = (message: Message) => (
-    <article
-      key={message.timestamp}
-      className={`flex w-full  border-b last-of-type:border-b-0 border-white/20 py-8 relative group ${
-        message.role === "user" ? "justify-end" : "justify-start"
-      }`}
-    >
-      <div
-        className={`max-w-[85%] sm:max-w-[75%]   ${
-          message.role === "user"
-            ? " text-white ml-auto"
-            : " text-gray-200 mr-auto"
-        }`}
-      >
-        <header
-          className={`absolute top-0 inset-x-0 md:opacity-0 md:group-hover:opacity-100 md:-translate-y-2 md:group-hover:translate-y-0 transition ${
-            message.role === "user" && "text-right"
-          }`}
-        >
-          <span className="whitespace-nowrap text-xs text-neutral-400 ">
-            {new Date(message.timestamp).toLocaleTimeString()}
-          </span>
-        </header>
-        <p className="text-sm sm:text-base break-words">{message.content}</p>
-        <footer>
-          {message.role === "agent" && (
-            <span className="uppercase tracking-widest text-xs text-pink-400">
-              — Dova
-            </span>
-          )}
-        </footer>
-        {/* <div className="mt-4">
-          <fieldset className="border border-neutral-600 rounded-xl p-4 bg-black overflow-hidden relative">
-            <legend className="uppercase text-xs tracking-widest ">
-              Agent Configurations
-            </legend>
-            <div>
-              <label>
-                <span>Name</span>
-                <input
-                  type="text"
-                  value="Mr. Love"
-                  readOnly
-                  className="form-input w-full bg-transparent rounded-lg text-sm"
-                />
-              </label>
-            </div>
-            <div>
-              <label>
-                <span>Description</span>
-                <input
-                  type="text"
-                  value="Lorem ipsum dolor sit amet consectetur, adipisicing elit. "
-                  readOnly
-                  className="form-input w-full bg-transparent rounded-lg text-sm"
-                />
-              </label>
-            </div>
-            <div className=" absolute inset-0 from-black to-transparent bg-gradient-to-t">
-              <button className=" text-blue-400 flex h-full w-full items-center justify-center uppercase">
-                open
-              </button>
-            </div>
-          </fieldset>
-        </div> */}
-      </div>
-    </article>
-  );
-
   const lastEl = useRef<HTMLDivElement | null>(null);
   useEffect(() => {
-    if (isFetched && lastEl.current) {
+    if (!isLoading && lastEl.current) {
       lastEl.current.scrollIntoView({ behavior: "instant" });
     }
-  }, [isFetched]);
+  }, [isLoading]);
 
   return (
     <div className="fixed inset-0 z-10 flex flex-col md:right-0 md:w-1/2 md:left-auto max-h-screen">
       <div className="flex-1 overflow-auto">
-        <div className=" space-y-4 h-full overflow-auto p-8">
-          {chatLog.map(renderMessage)}
+        <div className="h-full px-8">
+          {chatLog.map((message, i) => (
+            <article
+              key={message.timestamp}
+              className={`flex flex-col w-full relative group ${
+                message.role === "user" ? "justify-end" : "justify-start"
+              }`}
+            >
+              <ChatMessage message={message} />
+              {chatLog.length - 1 > i && (
+                <hr className=" border-white/20 w-24 mx-auto" />
+              )}
+            </article>
+          ))}
           <div ref={lastEl}></div>
         </div>
       </div>
@@ -100,3 +44,65 @@ export const ChatWindow = () => {
     </div>
   );
 };
+
+const ChatMessage = ({ message }: { message: Message }) => (
+  <div
+    className={`max-w-[85%] sm:max-w-[75%] py-8 ${
+      message.role === "user"
+        ? "text-neutral-300 ml-auto"
+        : "text-white mr-auto"
+    }`}
+  >
+    <header
+      className={`absolute top-2 inset-x-0 opacity-0 group-hover:opacity-100 -translate-y-2 group-hover:translate-y-0 transition ${
+        message.role === "user" && "text-right"
+      }`}
+    >
+      <span className="whitespace-nowrap text-xs text-neutral-400 ">
+        {formatDistanceToNow(message.timestamp, { addSuffix: true })}
+      </span>
+    </header>
+    <p className="text-sm break-words">{message.content}</p>
+    <footer>
+      {message.role === "agent" && (
+        <span className="uppercase tracking-widest text-[10px] text-pink-400">
+          — Dova
+        </span>
+      )}
+    </footer>
+    {/* <div className="mt-4">
+        <fieldset className="border border-neutral-600 rounded-xl p-4 bg-black overflow-hidden relative">
+          <legend className="uppercase text-xs tracking-widest ">
+            Agent Configurations
+          </legend>
+          <div>
+            <label>
+              <span>Name</span>
+              <input
+                type="text"
+                value="Mr. Love"
+                readOnly
+                className="form-input w-full bg-transparent rounded-lg text-sm"
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              <span>Description</span>
+              <input
+                type="text"
+                value="Lorem ipsum dolor sit amet consectetur, adipisicing elit. "
+                readOnly
+                className="form-input w-full bg-transparent rounded-lg text-sm"
+              />
+            </label>
+          </div>
+          <div className=" absolute inset-0 from-black to-transparent bg-gradient-to-t">
+            <button className=" text-blue-400 flex h-full w-full items-center justify-center uppercase">
+              open
+            </button>
+          </div>
+        </fieldset>
+      </div> */}
+  </div>
+);
